42dc21afb2e8b88e7116bfc42864ff4e367fdd7c
Benammar-omar <144702417+Benammar-omar@users.noreply.github.com>
2024-01-05 09:47:55 +0100

    cypress.config

--- cypress/e2e/Utilities/Hooks.js
+++ cypress/e2e/Utilities/Hooks.js
@@ -1,6 +1,6 @@
 
-import HomePage from "C:/Users/obenammar/Desktop/CypressTuto-Azmi/cypress/pages/HomePage.js";
-import Login from "C:/Users/obenammar/Desktop/CypressTuto-Azmi/cypress/pages/Login.js";
+import HomePage from "cypress/pages/HomePage.js";
+import Login from "cypress/pages/Login.js";
 
 
 const home = new HomePage()
